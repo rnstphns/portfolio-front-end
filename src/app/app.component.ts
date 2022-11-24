348ce388cf08ad967ac3306d2fb92b4bf4a1996c
rnstphns <97855804+rnstphns@users.noreply.github.com>
2022-11-23 21:28:51 -0600

    redirect to s3 with signed url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-front-end';
+
+  constructor(private http: HttpClient){}
+  
+  redirectToBlog(){
+    this.http.get(environment.blogUrl)
+                .subscribe(res => {
+                  const url = res.toString()
+                  window.open(url, "_blank");
+                })
+  }
+  
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  blogUrl: 'https://0ks7nk18u5.execute-api.us-east-1.amazonaws.com/default/BlogSignedURL'
 };
